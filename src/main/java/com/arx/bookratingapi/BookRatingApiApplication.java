dd85c4a48dddb36b28be4f3c93f91d3a8c2306da
ArxLancifer <a_christoforidis@hotmail.com>
2026-07-03 14:58:32 +0300

    Init spring boot project

---
+++ src/main/java/com/arx/bookratingapi/BookRatingApiApplication.java
@@ -0,0 +1,13 @@
+package com.arx.bookratingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRatingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRatingApiApplication.class, args);
+    }
+
+}
