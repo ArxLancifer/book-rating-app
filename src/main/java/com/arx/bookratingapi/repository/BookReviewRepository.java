0b8655afc185c7c648dbc0c82cf889397e18e8f8
ArxLancifer <a_christoforidis@hotmail.com>
2026-07-04 18:17:51 +0300

    Create jpa native query

--- src/main/java/com/arx/bookratingapi/repository/BookReviewRepository.java
+++ src/main/java/com/arx/bookratingapi/repository/BookReviewRepository.java
@@ -2,7 +2,26 @@ package com.arx.bookratingapi.repository;
 
 import com.arx.bookratingapi.model.entity.BookReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookReviewRepository extends JpaRepository<BookReview, Long> {
+
+    @Query(
+    value = """
+    SELECT 
+        br.id,
+        br.book_id, 
+        br.review, 
+        br.created_on,
+        br.is_deleted,
+        br.updated_on,
+        ROUND(AVG(rating) OVER (PARTITION BY book_id)::NUMERIC, 2) AS rating 
+    FROM book_reviews.book_review br
+    WHERE book_id = :bookId
+    """,
+    nativeQuery = true)
+    List<BookReview> getBookReviewsByBookIdAndAvgRate(Long bookId);
 
 }
