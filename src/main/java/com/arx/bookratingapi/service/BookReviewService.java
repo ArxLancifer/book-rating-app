4a3af079b74ea088fb90ddadff5374da95ab79d5
ArxLancifer <a_christoforidis@hotmail.com>
2026-07-04 16:11:01 +0300

    Remove unused imports

--- src/main/java/com/arx/bookratingapi/service/BookReviewService.java
+++ src/main/java/com/arx/bookratingapi/service/BookReviewService.java
@@ -1,7 +1,6 @@
 package com.arx.bookratingapi.service;
 
 import com.arx.bookratingapi.model.dto.BookReviewCommand;
-import com.arx.bookratingapi.model.dto.GutendexBookResponse;
 import com.arx.bookratingapi.repository.BookReviewRepository;
 import org.springframework.stereotype.Service;
 
