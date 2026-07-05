0773e63329bdebfd24d0360437c8e861d7584cbf
ArxLancifer <a_christoforidis@hotmail.com>
2026-07-05 17:51:58 +0300

    Quick fix

--- src/test/java/com/arx/bookratingapi/controller/BookSearchControllerE2ETest.java
+++ src/test/java/com/arx/bookratingapi/controller/BookSearchControllerE2ETest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -28,7 +27,6 @@ import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @EnableWireMock(
     @ConfigureWireMock(name = "mock-book-service", port = 8081)
